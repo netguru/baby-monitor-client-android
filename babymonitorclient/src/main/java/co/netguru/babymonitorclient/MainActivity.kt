6944abda900f17487fbc3dd895fe2c655876653b
Rafal Adasiewicz <rafal.adasiewicz@netguru.pl>
2018-09-06 12:52:19 +0200

    Add client

--- babymonitorclient/src/main/java/co/netguru/babymonitorclient/MainActivity.kt
+++ babymonitorclient/src/main/java/co/netguru/babymonitorclient/MainActivity.kt
@@ -1,12 +1,107 @@
 package co.netguru.babymonitorclient
 
-import android.support.v7.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.DisplayMetrics
+import android.view.View
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.ext.rtmp.RtmpDataSourceFactory
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
+import kotlinx.android.synthetic.main.activity_main.*
+import org.videolan.libvlc.LibVLC
+import org.videolan.libvlc.Media
+import org.videolan.libvlc.MediaPlayer
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var libvlc: LibVLC
+    private lateinit var mediaPlayer: MediaPlayer
+
+    private fun provideOptions() = mutableListOf<String>().apply {
+        add("--aout=opensles")
+        add("--audio-time-stretch") // time stretching
+        add("-vvv") //verbosity
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (SHOULD_PLAY_RTMP) {
+            prepareRtmpPlayer()
+        } else {
+            libvlc = LibVLC(this, provideOptions() as ArrayList<String>)
+            mediaPlayer = MediaPlayer(libvlc)
+            prepareRtspPlayer()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        releasePlayer()
+    }
+
+    private fun prepareRtmpPlayer() {
+        showSurfaceView(false)
+        val player = ExoPlayerFactory.newSimpleInstance(this, provideTrackSelector())
+        simplePlayer.player = player
+
+        with(player) {
+            prepare(provideVideoSource())
+            playWhenReady = true
+        }
+    }
+
+    private fun provideTrackSelector() =
+        DefaultTrackSelector(AdaptiveTrackSelection.Factory(DefaultBandwidthMeter()))
+
+    private fun provideVideoSource() = ExtractorMediaSource.Factory(RtmpDataSourceFactory())
+        .createMediaSource(Uri.parse(RTMP_VIDEO_URI))
+
+    private fun showSurfaceView(isVisible: Boolean) {
+        surfaceView.visibility = if (isVisible) View.VISIBLE else View.GONE
+        simplePlayer.visibility = if (isVisible) View.GONE else View.VISIBLE
+    }
+
+    private fun prepareRtspPlayer() {
+        showSurfaceView(true)
+
+        surfaceView.holder.setKeepScreenOn(true)
+        // Seting up video output
+        with(mediaPlayer.vlcVout) {
+            setVideoView(surfaceView)
+            val displayMetrics = DisplayMetrics()
+            windowManager.defaultDisplay.getMetrics(displayMetrics)
+            with(displayMetrics) {
+                setWindowSize(widthPixels, heightPixels)
+            }
+            attachViews()
+        }
+        with(mediaPlayer) {
+            media = Media(libvlc, Uri.parse(RTSP_VIDEO_URI))
+            aspectRatio = "16:9"
+            scale = 5f
+            play()
+        }
+    }
+
+    private fun releasePlayer() = with(mediaPlayer) {
+        stop()
+        vlcVout.detachViews()
+        libvlc.release()
+    }
+
+    companion object {
+        private const val SHOULD_PLAY_RTMP = false
+        private const val RTMP_VIDEO_URI =
+            "rtmp://stream1.livestreamingservices.com:1935/tvmlive/tvmlive"
+
+        private const val RTSP_VIDEO_URI =
+            "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov"
     }
 }
