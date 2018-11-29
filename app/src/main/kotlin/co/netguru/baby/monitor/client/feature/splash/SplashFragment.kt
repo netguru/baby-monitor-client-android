a5850e786140a63e9fe6c2fb0dca7190b97bb757
Mariusz Karwowski <karwowski.mar@gmail.com>
2018-11-29 10:57:05 +0100

    - splash screen - app functionality presentation

--- app/src/main/kotlin/co/netguru/baby/monitor/client/feature/splash/SplashFragment.kt
+++ app/src/main/kotlin/co/netguru/baby/monitor/client/feature/splash/SplashFragment.kt
@@ -3,14 +3,20 @@ package co.netguru.baby.monitor.client.feature.splash
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import co.netguru.baby.monitor.client.R
+import co.netguru.baby.monitor.client.feature.client.home.ChildData
+import co.netguru.baby.monitor.client.feature.client.home.ClientHomeActivity
 import co.netguru.baby.monitor.client.feature.client.home.ClientHomeViewModel
+import co.netguru.baby.monitor.client.feature.presentation.PresentationActivity
 import dagger.android.support.DaggerFragment
+import io.reactivex.Completable
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class SplashFragment : DaggerFragment() {
@@ -27,16 +33,28 @@ class SplashFragment : DaggerFragment() {
             savedInstanceState: Bundle?
     ) = inflater.inflate(R.layout.fragment_splash, container, false)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onResume() {
+        super.onResume()
         viewModel.refreshChildrenList()
-        viewModel.childList.observe(this, Observer { list ->
-            if (!list.isNullOrEmpty()) {
-                findNavController().navigate(R.id.actionSplashToClientHome)
-                requireActivity().finish()
-            } else {
-                findNavController().navigate(R.id.actionSplashToWelcome)
-            }
-        })
+        viewModel.childList.observe(this, Observer(this::handleSplash))
+    }
+
+    private fun handleSplash(list: List<ChildData>?) {
+        Completable
+                .timer(DELAY_MILLISECONDS, TimeUnit.MILLISECONDS)
+                .subscribeOn(Schedulers.io())
+                .subscribeBy {
+                    val intent = if (!list.isNullOrEmpty()) {
+                        Intent(requireContext(), ClientHomeActivity::class.java)
+                    } else {
+                        Intent(requireContext(), PresentationActivity::class.java)
+                    }
+                    startActivity(intent)
+                    requireActivity().finish()
+                }
+    }
+
+    companion object {
+        private const val DELAY_MILLISECONDS = 1000L
     }
 }
