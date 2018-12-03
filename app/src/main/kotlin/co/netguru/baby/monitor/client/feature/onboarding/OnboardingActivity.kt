7de3d4567fc8f7e404efbe431a33dbe863eb1752
Mariusz Karwowski <karwowski.mar@gmail.com>
2018-12-03 10:25:25 +0100

    fixed back press in server fragment

--- app/src/main/kotlin/co/netguru/baby/monitor/client/feature/onboarding/OnboardingActivity.kt
+++ app/src/main/kotlin/co/netguru/baby/monitor/client/feature/onboarding/OnboardingActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import androidx.navigation.findNavController
 import co.netguru.baby.monitor.client.R
+import co.netguru.baby.monitor.client.feature.server.ServerFragment
 
 class OnboardingActivity : AppCompatActivity() {
 
@@ -14,4 +15,13 @@ class OnboardingActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp() =
             findNavController(R.id.onboardingNavigationHostFragment).navigateUp()
+
+    override fun onBackPressed() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.onboardingNavigationHostFragment)
+        if (navHostFragment?.childFragmentManager?.fragments?.get(0) is ServerFragment) {
+            findNavController(R.id.onboardingNavigationHostFragment).navigate(R.id.onboardingServerToWelcome)
+        } else {
+            super.onBackPressed()
+        }
+    }
 }
