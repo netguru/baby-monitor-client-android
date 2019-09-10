f759694e163dbba3d0f9868af181822254633044
Michał Golda <michal.golda@netguru.com>
2019-09-10 10:06:39 +0200

    Add unique index on clients table to prevent duplicated notifications

--- app/src/main/kotlin/co/netguru/baby/monitor/client/feature/server/ReceiveFirebaseTokenUseCase.kt
+++ app/src/main/kotlin/co/netguru/baby/monitor/client/feature/server/ReceiveFirebaseTokenUseCase.kt
@@ -3,23 +3,9 @@ package co.netguru.baby.monitor.client.feature.server
 import co.netguru.baby.monitor.client.data.DataRepository
 import co.netguru.baby.monitor.client.data.communication.ClientEntity
 import io.reactivex.Completable
-import io.reactivex.Single
 import javax.inject.Inject
 
 class ReceiveFirebaseTokenUseCase @Inject constructor(private val dataRepository: DataRepository) {
-    fun receiveToken(ipAddress: String, token: String): Completable = Completable.fromAction {
-        dataRepository.getAllClientData()
-            .flatMapSingle { clientEntityList ->
-                Single.just(clientEntityList
-                    .firstOrNull { clientEntity ->
-                        clientEntity.address == ipAddress || clientEntity.firebaseKey == token
-                    }
-                    ?: ClientEntity(
-                        address = ipAddress,
-                        firebaseKey = token
-                    )
-                )
-            }
-            .flatMapCompletable(dataRepository::insertClientData)
-    }
+    fun receiveToken(ipAddress: String, token: String): Completable =
+        dataRepository.insertClientData(ClientEntity(address = ipAddress, firebaseKey = token))
 }
