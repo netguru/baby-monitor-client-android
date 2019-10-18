f5fc0a7f7d2b3156a2e44e7e2d6b324f6a6c0405
Mateusz <mateusz.gabrynowicz@netguru.com>
2019-10-18 10:48:11 +0200

    Tidying up (#141)
    * Update leakCanary
    
    * Delete unused dependencies and resources.
    
    Deleted unused dependencies and resources. Additionally made some
    refactor and formatting. Using gradle configuration, which specifically
    includes only armeabi-v7a and arm64-v8a, which should cover most
    modern Android devices as suggested in TensorFlow docs.
    
    * Delete unused dependencies and resources + danger suggested changes

--- app/src/main/kotlin/co/netguru/baby/monitor/client/data/communication/websocket/ConnectionStatus.kt
+++
@@ -1,5 +0,0 @@
-package co.netguru.baby.monitor.client.data.communication.websocket
-
-enum class ConnectionStatus {
-    UNKNOWN, CONNECTED, DISCONNECTED, RETRYING
-}
--- app/src/main/kotlin/co/netguru/baby/monitor/client/feature/communication/webrtc/base/WebRtcBinder.kt
+++
@@ -1,7 +0,0 @@
-package co.netguru.baby.monitor.client.feature.communication.webrtc.base
-
-import android.os.Binder
-
-abstract class WebRtcBinder: Binder() {
-    abstract fun cleanup()
-}
