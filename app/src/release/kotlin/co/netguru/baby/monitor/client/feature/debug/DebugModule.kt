a089e65afe6044fe8b2c4847ffd33998441cf079
Mateusz <mateusz.gabrynowicz@netguru.com>
2019-12-09 19:41:21 +0100

    Fix release build

--- app/src/release/kotlin/co/netguru/baby/monitor/client/feature/debug/DebugModule.kt
+++ app/src/release/kotlin/co/netguru/baby/monitor/client/feature/debug/DebugModule.kt
@@ -1,10 +1,10 @@
 package co.netguru.baby.monitor.client.feature.debug
 
-import android.content.Context
-import co.netguru.baby.monitor.client.application.DebugNotificationManager
+import io.reactivex.Observable
 import javax.inject.Inject
 
 class DebugModule @Inject constructor() {
-    fun initialize(context: Context) {}
-    fun clear(context: Context) {}
+    fun sendCryingProbabilityEvent(cryingProbability: Float) {}
+    fun sendNotificationEvent(notificationInformation: String) {}
+    fun debugStateObservable(): Observable<DebugState> = Observable.empty()
 }
