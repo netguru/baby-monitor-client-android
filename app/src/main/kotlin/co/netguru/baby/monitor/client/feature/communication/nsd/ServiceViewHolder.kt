389cc1ef54c2c6d12305d4ffefd1f61df61d8b51
Mateusz <mateusz.gabrynowicz@netguru.com>
2020-01-02 15:00:42 +0100

    Show "Unknown device name" on empty name.

--- app/src/main/kotlin/co/netguru/baby/monitor/client/feature/communication/nsd/ServiceViewHolder.kt
+++ app/src/main/kotlin/co/netguru/baby/monitor/client/feature/communication/nsd/ServiceViewHolder.kt
@@ -3,12 +3,16 @@ package co.netguru.baby.monitor.client.feature.communication.nsd
 import android.net.nsd.NsdServiceInfo
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import co.netguru.baby.monitor.client.R
 import kotlinx.android.synthetic.main.found_service_item.view.*
 
 class ServiceViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     fun bind(nsdServiceInfo: NsdServiceInfo) {
+        val deviceName = nsdServiceInfo.serviceName.removeSuffix(NsdServiceManager.SERVICE_NAME)
         itemView.deviceName.text =
-            nsdServiceInfo.serviceName.removeSuffix(NsdServiceManager.SERVICE_NAME)
+            if (deviceName.isNotEmpty()) deviceName else itemView.resources.getString(
+                R.string.unknown_device
+            )
     }
 }
