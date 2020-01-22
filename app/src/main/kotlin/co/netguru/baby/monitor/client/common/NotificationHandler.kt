add1e10fbfa41facf76cad7ab5663b3b32354252
Mateusz <mateusz.gabrynowicz@netguru.com>
2020-01-22 09:56:59 +0100

    Use extension function

--- app/src/main/kotlin/co/netguru/baby/monitor/client/common/NotificationHandler.kt
+++ app/src/main/kotlin/co/netguru/baby/monitor/client/common/NotificationHandler.kt
@@ -10,9 +10,9 @@ import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
-import androidx.core.content.ContextCompat
 import co.netguru.baby.monitor.client.R
 import co.netguru.baby.monitor.client.common.extensions.addActions
+import co.netguru.baby.monitor.client.common.extensions.getColorCompat
 import co.netguru.baby.monitor.client.feature.client.home.ClientHomeActivity
 import co.netguru.baby.monitor.client.feature.babycrynotification.CryingActionIntentService
 import co.netguru.baby.monitor.client.feature.server.ServerActivity
@@ -67,7 +67,7 @@ class NotificationHandler(private val context: Context) {
         return notificationBuilder
             .setContentTitle(title)
             .setContentText(content)
-            .setColor(ContextCompat.getColor(context, R.color.accent))
+            .setColor(context.getColorCompat(R.color.accent))
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setContentIntent(resultPendingIntent)
             .setAutoCancel(true)
