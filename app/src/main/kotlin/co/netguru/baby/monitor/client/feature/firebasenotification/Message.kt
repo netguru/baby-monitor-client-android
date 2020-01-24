bb74427591369676a6067e0b9aaa72ecf88452ee
Mateusz <mateusz.gabrynowicz@netguru.com>
2020-01-24 08:05:37 +0100

    Missing commit with @Serialized name

--- app/src/main/kotlin/co/netguru/baby/monitor/client/feature/firebasenotification/Message.kt
+++ app/src/main/kotlin/co/netguru/baby/monitor/client/feature/firebasenotification/Message.kt
@@ -5,17 +5,23 @@ import com.google.gson.annotations.SerializedName
 data class Message(
     @SerializedName("to")
     val firebaseToken: String,
+    @SerializedName("notification")
     val notification: Notification? = null,
+    @SerializedName("data")
     val data: Data? = null
 )
 
 data class Notification(
+    @SerializedName("title")
     val title: String,
+    @SerializedName("text")
     val text: String
 )
 
 data class Data(
+    @SerializedName("title")
     val title: String,
+    @SerializedName("text")
     val text: String,
     @SerializedName("notification_type")
     val type: String
