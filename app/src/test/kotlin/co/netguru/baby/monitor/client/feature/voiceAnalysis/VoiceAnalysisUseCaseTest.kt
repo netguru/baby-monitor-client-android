730d5571735cb5cb49dff3a502b35806b4e1ff99
Mateusz <mateusz.gabrynowicz@netguru.com>
2020-02-05 15:10:20 +0100

    Add initial voice option sending to be sure that baby:parent are aligned
    Also some refactor: delete unused class, addTo to +=, error handling

--- app/src/test/kotlin/co/netguru/baby/monitor/client/feature/voiceAnalysis/VoiceAnalysisUseCaseTest.kt
+++ app/src/test/kotlin/co/netguru/baby/monitor/client/feature/voiceAnalysis/VoiceAnalysisUseCaseTest.kt
@@ -3,10 +3,13 @@ package co.netguru.baby.monitor.client.feature.voiceAnalysis
 import co.netguru.baby.monitor.client.common.ISchedulersProvider
 import co.netguru.baby.monitor.client.common.Randomiser
 import co.netguru.baby.monitor.client.data.DataRepository
+import co.netguru.baby.monitor.client.data.client.ChildDataEntity
 import co.netguru.baby.monitor.client.feature.communication.websocket.Message
 import co.netguru.baby.monitor.client.feature.communication.websocket.MessageController
+import co.netguru.baby.monitor.client.feature.communication.websocket.RxWebSocketClient
 import com.nhaarman.mockitokotlin2.*
 import io.reactivex.Completable
+import io.reactivex.Maybe
 import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
 import io.reactivex.schedulers.TestScheduler
@@ -21,6 +24,9 @@ class VoiceAnalysisUseCaseTest {
     private val randomDigitsList = listOf(1, 2, 3, 4)
     private val confirmationId = randomDigitsList.joinToString("")
     private val timerTestScheduler = TestScheduler()
+    private val rxWebSocketClient: RxWebSocketClient = mock {
+        on { send(any()) }.doReturn(Completable.complete())
+    }
     private val schedulersProvider = mock<ISchedulersProvider> {
         on { io() } doReturn timerTestScheduler
         on { mainThread() } doReturn Schedulers.trampoline()
@@ -56,8 +62,10 @@ class VoiceAnalysisUseCaseTest {
         ).test()
             .assertComplete()
 
-        verify(messageController).sendMessage(argThat { voiceAnalysisOption ==
-                VoiceAnalysisOption.MachineLearning.name })
+        verify(messageController).sendMessage(argThat {
+            voiceAnalysisOption ==
+                    VoiceAnalysisOption.MachineLearning.name
+        })
     }
 
     @Test
@@ -68,8 +76,10 @@ class VoiceAnalysisUseCaseTest {
         ).test()
             .assertValue(true)
 
-        verify(messageController).sendMessage(argThat { voiceAnalysisOption ==
-                VoiceAnalysisOption.MachineLearning.name })
+        verify(messageController).sendMessage(argThat {
+            voiceAnalysisOption ==
+                    VoiceAnalysisOption.MachineLearning.name
+        })
     }
 
     @Test
@@ -89,8 +99,10 @@ class VoiceAnalysisUseCaseTest {
             .assertValue(false)
 
         timerTestScheduler.advanceTimeBy(5, TimeUnit.SECONDS)
-        verify(messageController).sendMessage(argThat { voiceAnalysisOption ==
-                VoiceAnalysisOption.MachineLearning.name })
+        verify(messageController).sendMessage(argThat {
+            voiceAnalysisOption ==
+                    VoiceAnalysisOption.MachineLearning.name
+        })
     }
 
     @Test
@@ -120,5 +132,23 @@ class VoiceAnalysisUseCaseTest {
             .assertValue(false)
 
         verifyZeroInteractions(dataRepository)
+    }
+
+    @Test
+    fun `should sent initial option`() {
+        val initialOption = VoiceAnalysisOption.MachineLearning
+        val childData = mock<ChildDataEntity> {
+            on { voiceAnalysisOption }.doReturn(initialOption)
+        }
+        whenever(dataRepository.getChildData()).doReturn(Maybe.just(childData))
+        voiceAnalysisUseCase
+            .sendInitialVoiceAnalysisOption(rxWebSocketClient)
+            .test()
+            .assertComplete()
+
+        verify(dataRepository).getChildData()
+        verify(rxWebSocketClient).send(check {
+            assert(it.voiceAnalysisOption == initialOption.name)
+        })
     }
 }
